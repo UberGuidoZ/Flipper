cc6b1990ea9a804b121139738388a0fc3b5c2259
RogueMaster <RogueMaster@users.noreply.github.com>
2024-03-24 21:35:38 -0400

    RM FAP UPDATE

--- Applications/Custom (UL, RM, XFW)/RogueMaster/Scripts/storage.js
+++ Applications/Custom (UL, RM, XFW)/RogueMaster/Scripts/storage.js
@@ -4,9 +4,12 @@ let path = "/ext/storage.test";
 print("File exists:", storage.exists(path));
 
 print("Writing...");
-storage.write(path, "Hello World!");
+storage.write(path, "Hello ");
 
 print("File exists:", storage.exists(path));
+
+// Append will create the file even if it doesnt exist!
+storage.append(path, "World!");
 
 print("Reading...");
 let data = storage.read(path);
--- Applications/Custom (UL, RM, XFW)/RogueMaster/Scripts/usbdisk.js
+++ Applications/Custom (UL, RM, XFW)/RogueMaster/Scripts/usbdisk.js
@@ -1,8 +1,9 @@
 let usbdisk = require("usbdisk");
+let image = "/ext/apps_assets/mass_storage/test.img";
 // print("Creating image...");
 // usbdisk.createImage("/ext/apps_data/mass_storage/128MB.img", 128 * 1024 * 1024);
 print("Starting UsbDisk...");
-usbdisk.start("/ext/apps_data/mass_storage/128MB.img");
+usbdisk.start(image);
 print("Started, waiting until ejected...");
 while (!usbdisk.wasEjected()) {
     delay(1000);
