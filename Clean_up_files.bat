20717e949ae10529c9c13767d079694f9c28492b
forid786 <forid9@gmail.com>
2024-10-19 22:56:24 +0100

    Update Clean_up_files.bat
    - Added deletion of PDFs
    - Cleanup Attributes 
    - Grouped deletions

--- Clean_up_files.bat
+++ Clean_up_files.bat
@@ -1,23 +1,14 @@
-echo off
+@echo off
 @title Flipper SD Cleaner
 cls
 cd /D "%~dp0"
-echo.
-echo  Continuing will DELETE FILES from %~dp0 and all subdirectories.
-echo  All .git, .mp4, .jp*, .png, .md, and README files will be removed!
-echo.
-echo  If you're good with that, press any key to continue. Otherwise, close out.
+echo This will DELETE the following file types from %~dp0 and all subdirectories:
+echo .git, .mp4, .jp*, .png, .md, .pdf, and README files.
+echo Press any key to continue, or close this window to abort.
 pause > NUL
-echo.
-echo  Deleting files, please wait...
-attrib *.* -h -s -r /s
-del *.git /s /q
-del *.mp4 /s /q
-del *.jp* /s /q
-del *.png /s /q
-del *.md /s /q
-del README /s /q
-echo.
-echo  Done! Press any key to exit. (Scroll up for errors if desired.)
+echo Deleting files, please wait...
+attrib -h -s -r /s /d *.*
+del /s /q *.git *.mp4 *.jp* *.png *.md *.pdf README
+echo Done! Press any key to exit.
 pause > NUL
 exit
