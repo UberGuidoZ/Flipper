5a036bcfd575da9b0d10237d90ab0301e848dfd7
UberGuidoZ <57457139+UberGuidoZ@users.noreply.github.com>
2024-10-22 21:47:51 -0700

    Minor tweaks to wording

--- Clean_up_files.bat
+++ Clean_up_files.bat
@@ -2,13 +2,18 @@
 @title Flipper SD Cleaner
 cls
 cd /D "%~dp0"
-echo This will DELETE the following file types from %~dp0 and all subdirectories:
-echo .git, .mp4, .jp*, .png, .md, .pdf, and README files.
-echo Press any key to continue, or close this window to abort.
+echo.
+echo  This will DELETE the following file types from %~dp0 and all subdirectories:
+echo.
+echo  .git, .mp4, .jp*, .png, .md, .pdf, and README files.
+echo.
+echo  If you're good with that, press any key to continue. Otherwise, X out to abort.
 pause > NUL
-echo Deleting files, please wait...
+echo.
+echo  Deleting files, please wait...
 attrib -h -s -r /s /d *.*
 del /s /q *.git *.mp4 *.jp* *.png *.md *.pdf README
-echo Done! Press any key to exit.
+echo.
+echo  Done! Press any key to exit. (Scroll up for errors if desired.)
 pause > NUL
 exit
