01b5579183765feafe721e13b56b67e3dc668f35
RogueMaster <RogueMaster@users.noreply.github.com>
2025-02-16 00:33:53 -0500

    RM Changes Part 1

--- Applications/Custom (UL, RM)/RogueMaster/Scripts/path.js
+++
@@ -1,9 +0,0 @@
-let storage = require("storage");
-
-print("script has __dirpath of" + __dirpath);
-print("script has __filepath of" + __filepath);
-if (storage.exists(__dirpath + "/math.js")) {
-    print("math.js exist here.");
-} else {
-    print("math.js does not exist here.");
-}
